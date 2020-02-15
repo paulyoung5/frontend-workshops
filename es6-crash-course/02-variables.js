fe96f3a66492291ed7df3eb72838e2eee32b68fd
Paul Young <paul-young@live.co.uk>
2020-02-15 12:13:18 +0000

    add stub files

---
+++ es6-crash-course/02-variables.js
@@ -0,0 +1,11 @@
+var x = 10;
+let y = 3;
+const z = 4;
+
+/**
+ * What is the difference between the above?
+ * 
+ * It's about 'scope'
+ */
+
+ 
---
+++ es6-crash-course/03-arrow-functions.js
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------/
+ * Arrow functions
+ * AKA: "fat arrows", "anonymous functions"
+ * 
+ * Pros:
+ * - Compact syntax means *some* operations can be
+ *   expressed more readably
+ * - Preserves 'this' - no need for a `self` variable,
+ *   or `.bind(this)`
+ * 
+ * Cons:
+ * - Shorter code isn't always more readable
+ * - Reading the call stack when debugging isn't so informative
+ *
+/-----------------------------------------------------------------------*/
+ 
+/**
+ * Example: 
+ */
+function 
